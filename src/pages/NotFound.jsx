cf88d91b772d29350bc056577fa6091296f777a1
Javier Collazos <jcollazosgomez@gmail.com>
2021-04-18 18:09:25 -0500

    Fixing 404 page titles

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 class NotFound extends React.Component{
     render(){
-        return <div className="h-screen flex flex-col justify-center items-center">
+        return <div className="h-screen flex flex-col justify-center items-center text-center">
             <img src="https://i.imgur.com/OjiAoAK.png" alt=""/>
-            <h1 className="text-2xl font-semibold font-nunito">¡You just catched a 404'mon behind that bush!</h1>
-            <p className="text-lg font-normal font-nunito">By the way, there is nothing else here... you can go <Link to ="/"><span className="font-semibold text-blue">home</span></Link> if you want..</p>
+            <h1 className="text-2xl font-semibold font-nunito pl-8 pr-8">¡You just catched a 404'mon behind that bush!</h1>
+            <p className="text-lg font-normal font-nunito pl-8 pr-8">By the way, there is nothing else here... you can go <Link to ="/"><span className="font-semibold text-blue">home</span></Link> if you want..</p>
         </div>;
     }
 }
